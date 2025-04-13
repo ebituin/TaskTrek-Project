11918aae2af000c5d8f89a956fd2db662fa7c673
eric <ebituin@gbox.adnu.edu.ph>
2025-04-14 00:39:28 +0800

    alignment

--- lib/pages/resetPassword.dart
+++ lib/pages/resetPassword.dart
@@ -69,69 +69,69 @@ class _forgot_ResetPageState extends State<forgot_Reset> {
               Column(
                 children: [
                   Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(
-                        height: 15,
-                        width: 15,
-                        child: Opacity(
-                          opacity: 0.8,
-                          child: Image.asset(
-                          'lib/assets/images/padlock.png',
+                      Column(
+                        children: [
+                          SizedBox(height: 15),
+                          SizedBox(
+                            height: 15,
+                            width: 15,
+                            child: Opacity(
+                              opacity: 0.8,
+                              child: Image.asset(
+                              'lib/assets/images/padlock.png',
+                              ),
+                            ),
                           ),
-                        ),
+                        ],
+                        
                       ),
-                      SizedBox(height: 10,)
+                      SizedBox(width: 5,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              'Confirm New Password',
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                          SizedBox(height: 5,),
+                          SizedBox(
+                            height: 38.0,
+                            width: 258,
+                            child: TextFormField(
+                              controller: _passwordController,
+                              style: TextStyle(fontSize: 14.0),
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                hintText: 'PenPineappleApplePen',
+                                suffixIcon: IconButton(
+                                  icon: Icon(_obscureText ? Icons.visibility_off : Icons.visibility),
+                                  onPressed: passwordVisibilitySate,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(6)
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10.0,
+                                  horizontal: 12.0,
+                                ),
+                              ),
+                              obscureText: _obscureText,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                
+                          
+                        ],
+                      )
                     ],
-                    
                   ),
-                  SizedBox(width: 5,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                          'Confirm New Password',
-                          style: TextStyle(
-                            fontSize: 15,
-                          ),
-                        ),
-                      SizedBox(height: 5,),
-                      SizedBox(
-                        height: 38.0,
-                        width: 258,
-                        child: TextFormField(
-                          controller: _passwordController,
-                          style: TextStyle(fontSize: 14.0),
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintText: 'PenPineappleApplePen',
-                            suffixIcon: IconButton(
-                              icon: Icon(_obscureText ? Icons.visibility_off : Icons.visibility),
-                              onPressed: passwordVisibilitySate,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(6)
-                            ),
-                            contentPadding: EdgeInsets.symmetric(
-                              vertical: 10.0,
-                              horizontal: 12.0,
-                            ),
-                          ),
-                          obscureText: _obscureText,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-            
-                      
-                    ],
-                  )
-                ],
-              ),
                   SizedBox(
-                    height: 30,
+                    height: 15,
                     child: Center(
                       child: emptyText ? 
                       Text(
@@ -148,82 +148,91 @@ class _forgot_ResetPageState extends State<forgot_Reset> {
                       ) : null
                     ),
                   ),
-                  Container(
-                    alignment: Alignment.topLeft,
-                    padding: EdgeInsets.symmetric(horizontal: 50),
-                    child: Row(
-                      children: [
-                        SizedBox(width: 44,),
-                        Text(
-                          'Confirm New Password',
-                          style: TextStyle(
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  SizedBox(height: 5,),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(
-                        height: 15,
-                        width: 15,
-                        child: Opacity(
-                          opacity: 0.8,
-                          child: Image.asset(
-                          'lib/assets/images/padlock.png',
-                          ),
-                        ),
+                      Column(
+                        children: [
+                          SizedBox(height: 15),
+                          SizedBox(
+                            height: 15,
+                            width: 15,
+                            child: Opacity(
+                              opacity: 0.8,
+                              child: Image.asset(
+                              'lib/assets/images/padlock.png',
+                              ),
+                            ),
+                          )
+                        ],
+                        
                       ),
-                      SizedBox(width: 5),
-                      SizedBox(
-                        height: 38.0,
-                        width: 258,
-                        child: TextFormField(
-                          controller: _confirmPasswordController,
-                          style: TextStyle(fontSize: 14.0),
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintText: 'PenPineappleApplePen',
-                            suffixIcon: IconButton(
-                              icon: Icon(_newObscureText ? Icons.visibility_off : Icons.visibility),
-                              onPressed: newPasswordVisibilitySate,
+                      SizedBox(width: 5,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              'Confirm New Password',
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
                             ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(6)
-                            ),
-                            contentPadding: EdgeInsets.symmetric(
-                              vertical: 10.0,
-                              horizontal: 12.0,
+                          SizedBox(height: 5,),
+                          SizedBox(
+                            height: 38.0,
+                            width: 258,
+                            child: TextFormField(
+                              controller: _confirmPasswordController,
+                              style: TextStyle(fontSize: 14.0),
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                hintText: 'PenPineappleApplePen',
+                                suffixIcon: IconButton(
+                                  icon: Icon(_newObscureText ? Icons.visibility_off : Icons.visibility),
+                                  onPressed: newPasswordVisibilitySate,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(6)
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10.0,
+                                  horizontal: 12.0,
+                                ),
+                              ),
+                              obscureText: _newObscureText,
                             ),
                           ),
-                          obscureText: _newObscureText,
-                        ),
-                      ),
+                          SizedBox(height: 10),
+                
+                          
+                        ],
+                      )
                     ],
                   ),
+                  SizedBox(
+                    height: 15,
+                    child: Center(
+                      child: confirmEmptyText ? 
+                      Text(
+                        'Password cannot be empty.',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 255, 0, 0), fontSize: 12,
+                        )
+                      ) : passwordNotMatch ? 
+                      Text(
+                        'Password do not matched. Please try again.',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 255, 0, 0), fontSize: 12,
+                        ) 
+                      ) : null
+                    ),
+                  ),
+                  SizedBox(height: 15,)
                 ],
               ),
-              SizedBox(
-                height: 30,
-                child: Center(
-                  child: confirmEmptyText ? 
-                  Text(
-                    'Password cannot be empty.',
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 255, 0, 0), fontSize: 12,
-                    )
-                  ) : passwordNotMatch ? 
-                  Text(
-                    'Password do not matched. Please try again.',
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 255, 0, 0), fontSize: 12,
-                    ) 
-                  ) : null
-                ),
-              ),
+              
               SizedBox(
                 width: 286,
                 height: 43,
@@ -234,7 +243,7 @@ class _forgot_ResetPageState extends State<forgot_Reset> {
                       confirmEmptyText = _confirmPasswordController.text.isEmpty;
                       passwordNotMatch = _passwordController.text != _confirmPasswordController.text;
                     });
-                    !passwordNotMatch ?
+                    !passwordNotMatch && !emptyText && !confirmEmptyText?
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => MyHomePage(),
