c9743d1c0e209571f8249bac9948c22c944c7d22
eric <ebituin@gbox.adnu.edu.ph>
2025-05-17 23:53:19 +0800

    renamed the dabase file

--- lib/pages/home/sign_up/signUpAccountType.dart
+++ lib/pages/home/sign_up/signUpAccountType.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:tasktrek/services/signUpDB.dart';
+import 'package:tasktrek/services/database.dart';
 import 'package:tasktrek/styles/styles.dart';
 
 class Signup extends StatefulWidget {
@@ -10,104 +10,90 @@ class Signup extends StatefulWidget {
   State<Signup> createState() => _SignUpPageState();
 }
 
-
-
-
-
-
-
 class _SignUpPageState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // Provides the basic structure for the signup screen.
-      backgroundColor:  AppColors.primaryColor,
-      body: 
-      Center(
-        child: 
-          Padding(
-            padding: const EdgeInsets.only(top: 70.0),
-            child: 
-            Column(
-              children: [
-                Padding(padding: const EdgeInsets.only(top: 70)),
-                Text(
-                  'Be part of a TaskTrek',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'Which user are you?',
-                  style: TextStyle( color: AppColors.accentColor),
-                ),
-                SizedBox(height: 20,),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  padding: EdgeInsets.all(12.0),
-                  child: Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/SignUpForm');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          iconColor: Colors.black,
-                          minimumSize: Size(double.infinity, 50),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                          backgroundColor:  AppColors.accentColor,
+    return Scaffold(
+      // Provides the basic structure for the signup screen.
+      backgroundColor: AppColors.primaryColor,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 70.0),
+          child: Column(
+            children: [
+              Padding(padding: const EdgeInsets.only(top: 70)),
+              Text(
+                'Be part of a TaskTrek',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Which user are you?',
+                style: TextStyle(color: AppColors.accentColor),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                padding: EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/SignUpForm');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        iconColor: Colors.black,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
                         ),
-                        child: Text(
-                          'Student',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
+                        backgroundColor: AppColors.accentColor,
                       ),
-                      SizedBox(height: 15),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'routeName');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          iconColor: Colors.black,
-                          minimumSize: Size(double.infinity, 50),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                          backgroundColor: const Color.fromARGB(255, 5, 10, 53),
-                        ),
-                        child: Text(
-                          'Teacher',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
+                      child: Text(
+                        'Student',
+                        style: TextStyle(color: Colors.white),
                       ),
-                      SizedBox(height: 15),
-                      ElevatedButton(
-                        onPressed: () {
-                        },
-                        style: ElevatedButton.styleFrom(
-                          iconColor: Colors.black,
-                          minimumSize: Size(double.infinity, 50),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                          backgroundColor: const Color.fromARGB(255, 5, 10, 53),
+                    ),
+                    SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'routeName');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        iconColor: Colors.black,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
                         ),
-                        child: Text(
-                          'Worker',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
+                        backgroundColor: const Color.fromARGB(255, 5, 10, 53),
                       ),
-                    ]
-                  ),
+                      child: Text(
+                        'Teacher',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        iconColor: Colors.black,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        backgroundColor: const Color.fromARGB(255, 5, 10, 53),
+                      ),
+                      child: Text(
+                        'Worker',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            )
-          )
-        ,
-      )
-      
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
